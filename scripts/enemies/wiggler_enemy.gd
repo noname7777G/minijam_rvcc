a9e13cf2fae0dfed943de2265db3ef7994f1a986
Calla Eris Orion <62214342+cheekylions@users.noreply.github.com>
2024-10-13 16:52:11 -0400

    Wiggler updates
    Additions to Wiggler

--- scripts/enemies/wiggler_enemy.gd
+++ scripts/enemies/wiggler_enemy.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-@onready var _head = $head
-@onready var _thorax = $thorax
-@onready var _abdomen = $abdomen
-@onready var _tail = $tail
+@onready var _body = $CharacterBody2D
+@onready var _head = $CharacterBody2D/head
+@onready var _thorax = $CharacterBody2D/thorax
+@onready var _abdomen = $CharacterBody2D/abdomen
+@onready var _tail = $CharacterBody2D/tail
+
+@onready var _animation_player = $CharacterBody2D/AnimationPlayer
 
 @export var health: float = 20.0
 @export var speed: float = 20.0
+
+var state = "idle"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +19,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if state == "search":
+		pass
+	
+	if state == "lunge":
+		pass
+		
+	if state == "turn":
+		pass
+		
+	if state == "move":
+		pass
 	
 func on_hit(damage: float):
 	health -= damage
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
